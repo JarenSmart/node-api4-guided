b9d165dde0d3f3679f56ca04e53078407b216257
jaren <jarensmart@yahoo.com>
2020-06-10 20:27:35 -0400

    Use heroku port env var

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
-const express = require("express")
-const helmet = require("helmet")
-const cors = require("cors")
-const welcomeRouter = require("./welcome/welcome-router")
-const shoutsRouter = require("./shouts/shouts-router")
+// A few different ways to stand up a web server with an IP address
 
-const server = express()
-const port = 4000
+// - We could self-host from our own house
+// - We could rent a server from a hosting company
+// - We could use a web hosting service (ex. Netlify, Zeit, Heroku)
 
-server.use(express.json())
-server.use(helmet())
-server.use(cors())
+// Website that outlines best methodology for deploying web apps. https://12factor.net/
 
-server.use("/", welcomeRouter)
-server.use("/shouts", shoutsRouter)
+// install cross-env --> npm i --save-dev cross-env
+
+// install dotenv --> npm i dotenv (this will help us hide sensitive info we don't want seen within our code)
+
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const welcomeRouter = require("./welcome/welcome-router");
+const shoutsRouter = require("./shouts/shouts-router");
+
+const server = express();
+const port = process.env.PORT || 4000;
+
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+server.use("/", welcomeRouter);
+server.use("/shouts", shoutsRouter);
 
 server.use((err, req, res, next) => {
-	console.log(err)
-	res.status(500).json({
-		message: "Something went wrong",
-	})
-})
+  console.log(err);
+  res.status(500).json({
+    message: "Something went wrong",
+  });
+});
 
 server.listen(port, () => {
-	console.log(`Server running at http://localhost:${port}`)
-})
+  console.log(`Server running at http://localhost:${port}`);
+});
